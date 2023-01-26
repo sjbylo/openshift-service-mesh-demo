d56662f5dc2f341f26ba4976950413b0875622f8
Stephen Bylo <stephenbylo@gmail.com>
2023-01-26 16:53:08 +0800

    Added https ingress using sds

--- 30-configure-https-ingress.sh
+++ 30-configure-https-ingress.sh
@@ -3,15 +3,18 @@
 [ "$DOM" ] || DOM=`oc get ingresses.config.openshift.io cluster -o jsonpath='{.spec.domain}'`
 
 cd config/certs/ingress 
+mkdir -p tmp
 
-openssl req -x509 -config cert.cfg -extensions req_ext -nodes -days 730 -newkey rsa:2048 -sha256 -keyout tls.key -out tls.crt
+# If you replace the example.com with something long you get an ssl error
+#sed "s/example.com/$DOM/g" < tmp/cert.cfg > tmp/cert.cfg
+openssl req -x509 -config tmp/cert.cfg -extensions req_ext -nodes -days 730 -newkey rsa:2048 -sha256 -keyout tmp/tls.key -out tmp/tls.crt
 
 oc get secret istio-ingressgateway-certs -n istio-system && oc delete secret istio-ingressgateway-certs -n istio-system 
 
-oc create secret tls istio-ingressgateway-certs --cert tls.crt --key tls.key -n istio-system
+oc create secret tls istio-ingressgateway-certs --cert tmp/tls.crt --key tmp/tls.key -n istio-system
 
 oc patch deployment istio-ingressgateway -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%FT%T%z`'"}}}}}' -n istio-system
 
-sed "s/FQDN/$DOM/g" < gateway.yaml | oc replace -f -
+sed "s/example.com/$DOM/g" < control-gateway.yaml | oc replace -f -
 
 
