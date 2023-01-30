9945bdeaf857d4a7db06ffbf7b787018ea095d8b
Stephen Bylo <stephenbylo@gmail.com>
2023-01-30 12:35:32 +0800

    sds fix

--- 40-configure-https-ingress.sh
+++ 40-configure-https-ingress.sh
@@ -10,7 +10,7 @@ mkdir -p tmp
 openssl req -x509 -config tmp/cert.cfg -extensions req_ext -nodes -days 730 -newkey rsa:2048 -sha256 -keyout tmp/tls.key -out tmp/tls.crt
 
 oc get -n istio-system secret travel-control-credential && oc delete -n istio-system secret travel-control-credential 
-oc create -n istio-system secret tls travel-control-credential --key=tls.key --cert=tls.crt
+oc create -n istio-system secret tls travel-control-credential --key=tmp/tls.key --cert=tmp/tls.crt
 
 #oc get secret istio-ingressgateway-certs -n istio-system && oc delete secret istio-ingressgateway-certs -n istio-system 
 #oc create secret tls istio-ingressgateway-certs --cert tls.crt --key tls.key -n istio-system
