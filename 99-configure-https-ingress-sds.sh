9d5677aebf3608cd730b96fd06e11e0cc577e0ce
Stephen Bylo <stephenbylo@gmail.com>
2024-04-12 14:18:44 +0800

    Adding missing scripts

---
+++ 99-configure-https-ingress-sds.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+[ "$DOM" ] || DOM=`oc get ingresses.config.openshift.io cluster -o jsonpath='{.spec.domain}'`
+
+cd config/certs/ingress 
+mkdir -p tmp
+
+#sed "s/example.com/$DOM/g" < cert.cfg > tmp/cert.cfg
+
+openssl req -x509 -config tmp/cert.cfg -extensions req_ext -nodes -days 730 -newkey rsa:2048 -sha256 -keyout tmp/tls.key -out tmp/tls.crt
+
+oc get -n travel-control secret travel-control-credential && oc delete -n travel-control secret travel-control-credential 
+oc create -n travel-control secret tls travel-control-credential --key=tmp/tls.key --cert=tmp/tls.crt
+
+#oc get secret istio-ingressgateway-certs -n travel-control && oc delete secret istio-ingressgateway-certs -n travel-control 
+#oc create secret tls istio-ingressgateway-certs --cert tls.crt --key tls.key -n travel-control
+#oc patch deployment istio-ingressgateway -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%FT%T%z`'"}}}}}' -n travel-control
+
+sed "s/example.com/$DOM/g" < control-gateway-sds.yaml | oc replace -f -
+
+
---
+++ 99-configure-https-ingress.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+[ "$DOM" ] || DOM=`oc get ingresses.config.openshift.io cluster -o jsonpath='{.spec.domain}'`
+
+cd config/certs/ingress 
+mkdir -p tmp
+
+# If you replace the example.com with something long you get an ssl error
+#sed "s/example.com/$DOM/g" < tmp/cert.cfg > tmp/cert.cfg
+openssl req -x509 -config tmp/cert.cfg -extensions req_ext -nodes -days 730 -newkey rsa:2048 -sha256 -keyout tmp/tls.key -out tmp/tls.crt
+
+oc get secret istio-ingressgateway-certs -n travel-control && oc delete secret istio-ingressgateway-certs -n travel-control 
+sleep 1
+
+oc create secret tls istio-ingressgateway-certs --cert tmp/tls.crt --key tmp/tls.key -n travel-control
+sleep 1
+
+oc patch deployment istio-ingressgateway -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%FT%T%z`'"}}}}}' -n travel-control
+
+sed "s/example.com/$DOM/g" < control-gateway.yaml | oc replace -f -
+
+
