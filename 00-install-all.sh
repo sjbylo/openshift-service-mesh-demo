2c53d72b7a240da571d56334511adf17aee2ef8b
Stephen Bylo <StephenBylo@gmail.com>
2024-10-24 10:05:08 +0800

    Was executing 'mesh3' scripts by mistake

--- 00-install-all.sh
+++ 00-install-all.sh
@@ -5,8 +5,18 @@
 
 echo Executing the following commands:
 
-execscripts=./0[1-9]*sh
-execscripts="$execscripts ./41-configure-gateway-injection.sh ./10-install-travels-services-v2-and-v3.sh ./11-add-per-portal-routing-to-travels-v1-v2-v3.sh ./20-inject-delay-into-flight-svc.sh"
+execscripts="\
+./01-install-operators.sh
+./02-install-smcp-basic.sh
+./03-install-travels-app.sh
+./04-enroll-projects-into-mesh.sh
+./05-inject-side-cars-into-all-pods-in-mesh.sh
+./06-add-gw-vs-dr.sh
+./41-configure-gateway-injection.sh
+./10-install-travels-services-v2-and-v3.sh
+./11-add-per-portal-routing-to-travels-v1-v2-v3.sh
+./20-inject-delay-into-flight-svc.sh
+"
 
 ls -1 $execscripts
 echo -n "Continue (y/n) [y]:"
