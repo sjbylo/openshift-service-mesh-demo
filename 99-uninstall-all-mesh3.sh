8333cf099555b43abe6ee0306e553dea0eb3cab4
Stephen Bylo <StephenBylo@gmail.com>
2025-09-22 11:54:11 +0800

    Filename change (2)

--- 99-uninstall-all-mesh3.sh
+++ 99-uninstall-all-mesh3.sh
@@ -3,7 +3,7 @@
 echo Executing the following commands:
 echo
 echo oc delete -Rf config/mesh3
-echo 94-delete-operators-mesh.sh
+echo 94-delete-operators-mesh3.sh
 echo
 echo -n "Continue (y/n) [y]:"
 read yn 
